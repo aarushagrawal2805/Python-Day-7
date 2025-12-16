9d401ea4b5c971fbcaee3c3f58be553edacca65f
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-16 15:15:10 +0530

    Login Page

---
+++ Login.py
@@ -0,0 +1,10 @@
+#Simple Login Validation
+try:
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+
+    if username != "admin" or password != "1234":
+        raise Exception("Invalid login")
+    print("Login successful")
+except Exception as e:
+    print(e)
